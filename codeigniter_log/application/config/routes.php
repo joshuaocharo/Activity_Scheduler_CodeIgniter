e18929b9ef86e07f5a4a0bf164b6de01a42c3202
jsscmaina <43575617+jsscmaina@users.noreply.github.com>
2018-10-21 13:34:08 +0300

    Update codeigniter_log/application/config/routes.php

--- codeigniter_log/application/config/routes.php
+++ codeigniter_log/application/config/routes.php
@@ -4,7 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['posts/(:any)'] = 'posts/view/$1';
 $route['posts']='posts/index';
 $route['default_controller'] = 'user';
-$route['home'] = 'user/home';
 $route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
